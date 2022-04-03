714a0216e656e36d0627e3739f85c8e2ade97267
sandriansyah <sandriansyah38@gmail.com>
2022-04-03 14:31:05 +0700

    update

--- seeders/20220403072736-profile.js
+++ seeders/20220403072736-profile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,23 +11,25 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-
     await queryInterface.bulkInsert(
-      'users',
+      'profiles',
       [
         {
-          email:'erma@gmail.com',
-          password: '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
-          status:'admin',
-          fullName:"erma suryani",
+          gender: 'female',
+          address: 'Banjaran',
+          fotoProfile: 'erma.jpeg',
+          phoneNumber: '123456789',
+          idUser: 1,
           createdAt:"12-03-2022",
           updatedAt:"12-03-2022"
-        },
-      ],    
+        }
+      ],
       {}
     )
   },
 
+
+  
   async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
