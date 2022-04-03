059c5c3171ebea44ca504975d0b7f330e95a6fdd
sandriansyah <sandriansyah38@gmail.com>
2022-04-03 14:57:32 +0700

    update

--- seeders/20220402223129-user.js
+++ seeders/20220402223129-user.js
@@ -13,20 +13,20 @@ module.exports = {
      * }], {});
     */
 
-    await queryInterface.bulkInsert(
-      'users',
-      [
-        {
-          email:'erma@gmail.com',
-          password: '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
-          status:'admin',
-          fullName:"erma suryani",
-          createdAt:"12-03-2022",
-          updatedAt:"12-03-2022"
-        },
-      ],    
-      {}
-    )
+    // await queryInterface.bulkInsert(
+    //   'users',
+    //   [
+    //     {
+    //       email:'erma@gmail.com',
+    //       password: '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
+    //       status:'admin',
+    //       fullName:"erma suryani",
+    //       createdAt:"12-03-2022",
+    //       updatedAt:"12-03-2022"
+    //     },
+    //   ],    
+    //   {}
+    // )
   },
 
   async down (queryInterface, Sequelize) {
